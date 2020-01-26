da483e87525a57981ea4aca052a1f8eb81a159ae
idriscahyono <idriscahyono@gmail.com>
2020-01-26 20:39:58 +0700

    first commit

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1580045579,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/dead-sheet0.png",
+		"images/dead-sheet1.png",
+		"images/dead-sheet2.png",
+		"images/acid-sheet0.png",
+		"images/groundgrass-sheet0.png",
+		"images/spike-sheet0.png",
+		"images/rockgrassdown-sheet0.png",
+		"images/rockgrass-sheet0.png",
+		"images/barrel-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
